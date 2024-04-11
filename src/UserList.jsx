ba30a8dba1e778523bd45b7c5185efb882e92055
shweta-rc13 <shwetachaurasiya1313@gmail.com>
2024-04-11 23:47:05 +0530

    fetch_data from API

---
+++ src/UserList.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import './UserList.css';
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error('Error fetching users:', error));
+  }, []);
+
+  return (
+    <div className='user-list-container'> 
+      <h1>User List</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <p>Name: {user.name}</p>
+            <p>Email: {user.email}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
